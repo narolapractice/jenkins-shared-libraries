f8f49ccc93177dd1bdb043bb9e5a4f04876bfaa5
narolapractice <practiceawsnarola@gmail.com>
2026-05-06 16:39:01 +0530

    Add Docker image tagging and pushing to Docker Hub functionality

---
+++ vars/push_to_dockerhub.groovy
@@ -0,0 +1,7 @@
+def call(String Project, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+  sh "docker image tag ${Project}:${ImageTag} ${dockerhubuser}/${Project}:${ImageTag}"
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+}
